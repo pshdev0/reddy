5066ad9bd6c75d0d24b411e7f40ab86a64a3ffb0
paul <pshdev0@gmail.com>
2024-12-29 16:12:55 +0000

    removed need for TypeReference
    added improved object type returning

--- src/main/java/com/pshdev0/reddy/Network.java
+++ src/main/java/com/pshdev0/reddy/Network.java
@@ -1,7 +1,6 @@
 package com.pshdev0.reddy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -13,10 +12,12 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.Security;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Network {
     private static Jedis jedis;
@@ -39,51 +40,8 @@ public class Network {
     }
 
     public static String createRedisKey(String source, String method, int version, String ... stringsToJoinAndHash) {
-        var id = String.join(":", stringsToJoinAndHash);
+        var id = Arrays.stream(stringsToJoinAndHash).map(x -> x.replaceAll("\\s+", "")).collect(Collectors.joining()).toLowerCase();
         return redisKeyPrefix + ":" + source + ":" + method + ":" + version + ":" + getKeccak256Hash(id);
-    }
-
-    public static <T> T getCachedOrComputeAndWait(Supplier<T> func,
-                                                  TypeReference<T> typeReference,
-                                                  String redisSourceName,
-                                                  String redisMethodName,
-                                                  int redisVersion,
-                                                  String ... stringsToMakeRedisHash) {
-        return getCachedOrComputeAndWait(func, typeReference, defaultMillis, redisSourceName, redisMethodName, redisVersion, stringsToMakeRedisHash);
-    }
-
-    public static <T> T getCachedOrComputeAndWait(Supplier<T> func,
-                                                  TypeReference<T> typeReference,
-                                                  long millisToWaitBeforeCompute,
-                                                  String redisSourceName,
-                                                  String redisMethodName,
-                                                  int redisVersion,
-                                                  String ... stringsToMakeRedisHash) {
-        var key = createRedisKey(redisSourceName, redisMethodName, redisVersion, stringsToMakeRedisHash);
-        var redis = getRedis();
-
-        if (redis.exists(key)) {
-            try {
-                var returnValue = objectMapper.readValue(redis.get(key), typeReference);
-                if (debugInfo) {
-                    System.out.println("redis key: " + key + " value: " + returnValue);
-                }
-                return returnValue;
-            } catch (JsonProcessingException e) {
-                System.out.println("Redis read failed, computing instead");
-            } catch (Exception ignored) {
-                System.out.println("failed to read redis value, computing...");
-            }
-        }
-
-        blockingWait(millisToWaitBeforeCompute);
-        var value = func.get();
-        try {
-            redis.set(key, objectMapper.writeValueAsString(value));
-        } catch (JsonProcessingException e) {
-            System.out.println("Could not set Redis key/value");
-        }
-        return value;
     }
 
     public static String getKeccak256Hash(String signature) {
@@ -105,72 +63,6 @@ public class Network {
         return hexString.toString();
     }
 
-    public static String post(String url, String requestBody) {
-        List<String> curlCommand = List.of(
-                "curl", "-X", "POST", url,
-                "-H", "Content-Type: application/json",
-                "-d", requestBody
-        );
-
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder(curlCommand);
-            Process process = processBuilder.start();
-
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                return response.toString();
-            }
-        } catch (IOException e) {
-            System.out.println("post error");
-        }
-
-        return null;
-    }
-
-    public static String getCachedOrPostAndWait(String sourceId, String url, String requestBody) {
-        return getCachedOrPostAndWait(sourceId, url, requestBody, defaultMillis);
-    }
-
-    public static String getCachedOrPostAndWait(String sourceId, String url, String requestBody, long millisToWaitBeforePost) {
-        return getCachedOrComputeAndWait(() -> post(url, requestBody), new TypeReference<>() {}, millisToWaitBeforePost, sourceId, "POST", 0, url, requestBody);
-    }
-
-    public static String get(String url) {
-        List<String> curlCommand = List.of("curl", "-X", "GET", url, "-H", "Content-Type: application/json");
-
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder(curlCommand);
-            Process process = processBuilder.start();
-
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-
-                return response.toString();
-            }
-
-        } catch (IOException e) {
-            System.out.println("get call error");
-        }
-
-        return null;
-    }
-
-    public static String getCachedOrGetAndWait(String sourceId, String url) {
-        return getCachedOrGetAndWait(sourceId, url, defaultMillis);
-    }
-
-    public static String getCachedOrGetAndWait(String sourceId, String url, long millisToWaitBeforeGet) {
-        return getCachedOrComputeAndWait(() -> Network.get(url), new TypeReference<>() {}, millisToWaitBeforeGet, sourceId, "GET", 0, url);
-    }
-
     public static ObjectMapper getObjectMapper() {
         return objectMapper;
     }
@@ -187,16 +79,135 @@ public class Network {
         CompletableFuture.runAsync(() -> {}, CompletableFuture.delayedExecutor(milliseconds, TimeUnit.MILLISECONDS)).join();
     }
 
-    public static <T> TypeReference<T> createJacksonTypeRef(Class<T> clazz) {
-        return new TypeReference<>() {
-            @Override
-            public java.lang.reflect.Type getType() {
-                return clazz;
-            }
-        };
-    }
-
     public static void showDebugInfo(boolean state) {
         debugInfo = state;
+    }
+
+    public static <T> T getCachedOrComputeAndWait(Supplier<T> func,
+                                                  Class<T> typeReference,
+                                                  String redisSourceName,
+                                                  String redisMethodName,
+                                                  int redisVersion,
+                                                  String ... stringsToMakeRedisHash) {
+        return getCachedOrComputeAndWait(func, typeReference, defaultMillis, redisSourceName, redisMethodName, redisVersion, stringsToMakeRedisHash);
+    }
+
+    public static <T> T getCachedOrComputeAndWait(Supplier<T> func,
+                                                  Class<T> clazz,
+                                                  long millisToWaitBeforeCompute,
+                                                  String redisSourceName,
+                                                  String redisMethodName,
+                                                  int redisVersion,
+                                                  String ... stringsToMakeRedisHash) {
+        var key = createRedisKey(redisSourceName, redisMethodName, redisVersion, stringsToMakeRedisHash);
+        var redis = getRedis();
+
+        if (redis.exists(key)) {
+            if (redis.get(key) == null) {
+                redis.del(key);
+            }
+            else {
+                try {
+                    var redisValue = redis.get(key);
+                    var returnValue = objectMapper.readValue(redisValue, clazz); // convert the JSON redis string to class T (if possible)
+                    if (debugInfo) {
+                        System.out.println("redis key: " + key + " value: " + returnValue);
+                        System.out.println("hashes:" + Arrays.stream(stringsToMakeRedisHash).map(x -> x.replaceAll("\\s+", "")).collect(Collectors.joining()).toLowerCase());
+                    }
+                    return returnValue;
+                } catch (JsonProcessingException e) {
+                    System.out.println("Redis read failed, computing instead");
+                } catch (Exception ignored) {
+                    System.out.println("failed to read redis value, computing instead");
+                }
+                redis.del(key); // something went wrong, delete the cached key/value pair
+            }
+        }
+
+        // at this point any previous key, if any, will no longer exist
+
+        blockingWait(millisToWaitBeforeCompute);
+        var value = func.get();
+
+        if (value == null) {
+            System.out.println("was going to use redis key " + key + ", but returned value was null");
+            System.out.println("hashes:" + Arrays.stream(stringsToMakeRedisHash).map(x -> x.replaceAll("\\s+", "")).collect(Collectors.joining()).toLowerCase());
+        }
+        else {
+            try {
+                var valueString = objectMapper.writeValueAsString(value); // convert object to string to store in redis as json string
+                redis.set(key, valueString); // store the key
+            } catch (JsonProcessingException e) {
+                System.out.println("Could not set Redis key/value, but returning non-null value anyway");
+                redis.del(key); // just to be safe, delete the key (although it should never be created)
+            }
+        }
+
+        return value;
+    }
+
+    public static <T> T post(String url, String requestBody, Class<T> returnClass) {
+        List<String> curlCommand = List.of(
+                "curl", "-X", "POST", url,
+                "-H", "Content-Type: application/json",
+                "-d", requestBody
+        );
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(curlCommand);
+            Process process = processBuilder.start();
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return objectMapper.readValue(response.toString(), returnClass);
+            }
+        } catch (IOException e) {
+            System.out.println("post error");
+        }
+
+        return null;
+    }
+
+    public static <T> T getCachedOrPostAndWait(String sourceId, String url, String requestBody, Class<T> returnClass) {
+        return getCachedOrPostAndWait(sourceId, url, requestBody, defaultMillis, returnClass);
+    }
+
+    public static <T> T getCachedOrPostAndWait(String sourceId, String url, String requestBody, long millisToWaitBeforePost, Class<T> returnClass) {
+        return getCachedOrComputeAndWait(() -> post(url, requestBody, returnClass), returnClass, millisToWaitBeforePost, sourceId, "POST", 0, url, requestBody);
+    }
+
+    public static <T> T get(String url, Class<T> returnClass) {
+        List<String> curlCommand = List.of("curl", "-X", "GET", url, "-H", "Content-Type: application/json");
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(curlCommand);
+            Process process = processBuilder.start();
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                return objectMapper.readValue(response.toString(), returnClass);
+            }
+        } catch (IOException e) {
+            System.out.println("get call error");
+        }
+
+        return null;
+    }
+
+    public static <T> T getCachedOrGetAndWait(String sourceId, String url, Class<T> returnClass) {
+        return getCachedOrGetAndWait(sourceId, url, defaultMillis, returnClass);
+    }
+
+    public static <T> T getCachedOrGetAndWait(String sourceId, String url, long millisToWaitBeforeGet, Class<T> returnClass) {
+        return getCachedOrComputeAndWait(() -> Network.get(url, returnClass), returnClass, millisToWaitBeforeGet, sourceId, "GET", 0, url);
     }
 }
